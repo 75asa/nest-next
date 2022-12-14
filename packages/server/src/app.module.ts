3c08782b6ed8b46005d883feb502fe6cf2dd2f6d
75asa <75asa.me@gmail.com>
2022-12-14 14:57:47 +0900

    feat: 🎸 unify client & server

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+const clientPath = `${__dirname}/../../client/out`;
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: clientPath,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
