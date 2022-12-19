7b450d87deafba7f0eedd452cd748d245ed3f617
75asa <75asa.me@gmail.com>
2022-12-19 13:05:47 +0900

    feat: 🎸 next-i18next-ssg

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import i18next from "i18next";
+// import "../i18n.init";
+import "../i18next/i18n.init";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
